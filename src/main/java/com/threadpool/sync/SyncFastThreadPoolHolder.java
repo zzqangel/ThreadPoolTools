25dd90113fe631fdfea2a59bd2d2bb9e2a8449e5
zhanzhiqiang <16969228@qq.com>
2023-03-21 15:49:26 +0800

    add private pool constructor

--- src/main/java/com/threadpool/sync/SyncFastThreadPoolHolder.java
+++ src/main/java/com/threadpool/sync/SyncFastThreadPoolHolder.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import java.util.List;
 import java.util.concurrent.Callable;
 
+/**
+ * this class is to offer a private sync thread pool not global
+ */
 public class SyncFastThreadPoolHolder implements Closeable {
 
     private final String threadPoolName;
 
     /**
-     * Please let this class be a static field to reuse
+     * you can let this class be a static field to reuse
      * If it's a local field, let it init in try-with-resources
      * @param threadPoolName the name of this thread pool, don't let it be conflicted and blank
      * @param threadPoolSize the size of this thread pool. The core size is equals to the max size
@@ -26,10 +29,20 @@ public class SyncFastThreadPoolHolder implements Closeable {
         this.threadPoolName = threadPoolName;
     }
 
+    /**
+     * to fire the jobs
+     * @param callableList jobs list
+     * @return the return of these jobs
+     * @param <T>
+     */
     public <T> List<T> doJobsNow(List<? extends Callable<T>> callableList) {
         return new SyncFastJob<>(callableList, threadPoolName).doJobsNow();
     }
 
+    /**
+     * If you want to close this pool, do with close method
+     * @throws IOException
+     */
     @Override
     public void close() throws IOException {
         SyncFastThreadPool.shutdown(this.threadPoolName);
